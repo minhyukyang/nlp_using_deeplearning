660255e11499754fe77d85836305e177c5c99bdf
minhyukyang <minhyukyang@gmail.com>
2019-03-19 23:22:53 +0900

    Revert "upload files and scripts"
    This reverts commit 752e5e6f1

--- scripts/01_Set the Environment.py
+++ scripts/01_Set the Environment.py
@@ -1,8 +1,28 @@
+# 1. 자연어 처리(natural language processing)란? ([링크](https://wikidocs.net/21667))
+
 '''
-1. 자연어 처리(natural language processing)란? - https://wikidocs.net/21667
-1) 자연어 처리 실습 환경 준비
-2) 딥 러닝 프레임워크와 필요 라이브러리
+## 1) 자연어 처리 실습 환경 준비
+1. 아나콘다(Anaconda) 설치
+2. nltk와 nltk data 설치
+3. nltk data 설치 시 에러가 발생할 경우 ([링크](https://github.com/nltk/nltk_data)]
+4. KoNLPy 설치
+5. 윈도우에서 KoNLPy 에러가 발생하는 경우
 '''
+
+'''
+## 2) 필요 프레임워크와 필요 라이브러리
+1. 텐서플로우(Tensorflow)
+2. 케라스(Keras)
+3. 넘파이(Numpy)
+4. 사이킷런(Scikit-learn)
+5. 판다스(Pandas)
+6. 주피터 노트북(Jupyter Notebook)
+'''
+
+'''
+## 4) 머신 러닝 워크플로우(Machine Learning Workflow)
+'''
+
 
 import nltk
 nltk.download()
